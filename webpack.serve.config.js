27894ac99d9edf3cd3ca6a2dce094e27b317f92b
Samuel Gratzl <sam@sgratzl.com>
2021-05-16 10:37:47 -0400

    build: tune demo build

--- webpack.serve.config.js
+++ webpack.serve.config.js
@@ -1,12 +1,14 @@
-const config = require('./webpack.config.js');
+/* eslint-disable import/extensions */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const config = require('./webpack.config.js');
 
-config.entry = { main: './src/demo/index.ts' };
+config.entry = { main: './src/demo/index.tsx' };
 config.output = {
   filename: './output.js',
   path: path.resolve(__dirname),
 };
 config.mode = 'development';
 config.externals = undefined; // eslint-disable-line
-config.devtool = 'inline-source-map';
+// config.devtool = 'inline-source-map';
 module.exports = config;

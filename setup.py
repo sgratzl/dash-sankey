4e344dc07ed6755aeb3ff8db576a7d9990887972
Samuel Gratzl <sam@sgratzl.com>
2021-05-24 11:35:40 -0400

    build: fix package setup to include JS files

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 import json
 from setuptools import setup
+from pathlib import Path
+from itertools import chain
 
 
 with open('package.json') as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
+base = Path(package_name)
+
+files = [f"{package_name}/{f.name}" for f in chain(base.glob('*.js'), base.glob('*.json'), base.glob('*.map'))]
 
 setup(
     name=package_name,
     version=package["version"],
     author=package['author'],
+    author_email="sam@sgratzl.com",
+    url=package['homepage'],
     packages=[package_name],
-    include_package_data=True,
+    data_files=[
+          (package_name, files),
+    ],
     license=package['license'],
     description=package.get('description', package_name),
     install_requires=[],
